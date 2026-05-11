72ad4a0b7f087dd1cd2993ad2d8217a000ee65cb
LironeFitoussi <lironefit@icloud.com>
2026-05-11 10:06:21 +0300

    feat: add link to Google in the main app component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -5,6 +5,11 @@ export default function App() {
     <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '4rem' }}>
       <h1>React App</h1>
       <p>Running in Kubernetes</p>
+      <div style={{ marginTop: '2rem' }}>
+        <a href="www.google.com" target="_blank" rel="noopener noreferrer">
+          Visit Google
+        </a>
+      </div>
     </div>
   )
 }
